4b6f7c48dde33b8d3197319369c13d328233a105
Matthew J Gao <mg00805@srcs.loc>
2017-11-20 11:45:32 -0800

    removed more unnecessary stuff, readability

--- functions.js
+++ functions.js
@@ -1,25 +1,25 @@
 // 1. Random Greeting
 
-  function randomGreeting() {
-        var salutation = ["Howdy!", "Greetings!", "Hola!", "Bonjour!", "Take me to your leader!", "!مرحبا", "你好！", "Здравствуйте!", "Sveiki!", "Алло!"];
-        var rand = Math.floor(Math.random() * 10);
-        return salutation[rand]
-  }
+function randomGreeting() {
+    var salutation = ["Howdy!", "Greetings!", "Hola!", "Bonjour!", "Take me to your leader!", "!مرحبا", "你好！", "Здравствуйте!", "Sveiki!", "Алло!"];
+    var rand = Math.floor(Math.random() * 10);
+    return salutation[rand]
+}
 
 // 2. Print Date
 
 function dateMessage() {
 
     var today = new Date();
-        var day = today.getDay();
-        var ddate = today.getDate();
-        var listday = ["Sunday", "Monday", "Tuesday", "Wedenesday", "Thursday", "Friday", "Saturday"]
-        var month = today.getMonth() + 1;
-        var listmonth = ["December", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November"]
-        var year = today.getFullYear();
-        document.write(" Today date is ", listday[day], " ", listmonth[month], " ", ddate + ", ", year);
+    var day = today.getDay();
+    var ddate = today.getDate();
+    var listday = ["Sunday", "Monday", "Tuesday", "Wedenesday", "Thursday", "Friday", "Saturday"]
+    var month = today.getMonth() + 1;
+    var listmonth = ["December", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November"]
+    var year = today.getFullYear();
+    document.write(" Today date is ", listday[day], " ", listmonth[month], " ", ddate + ", ", year);
 
-return "Today is " + day + ", " + month + " " + date + ", " + year;
+    return "Today is " + day + ", " + month + " " + ddate;
 
 }
 
@@ -31,31 +31,22 @@ function birthdaycountDown(datestring) {
     var today = new Date(),
         end = new Date(datestring),
 
-        date = today.getDate(),
-        datee = end.getDate(),
-
-        date = today.getDate(),
-        datee = end.getDate(),
-
-        year = today.getFullYear(),
-        yeare = end.getFullYear();
-
-    var duf = (end.getTime() - today.getTime()) / 86400000,
-        rduf = (end.getTime() - today.getTime()) % 86400000,
+        duf = (today.getTime() - end.getTime()) / (24 * 60 * 60 * 1000),
+        rduf = (today.getTime() - end.getTime()) % (24 * 60 * 60 * 1000),
         days = Math.floor(duf),
 
-        huf = rduf / 3600000,
-        rhuf = rduf % 3600000,
+        huf = rduf / (60 * 60 * 1000),
+        rhuf = rduf % (60 * 60 * 1000),
         hours = Math.floor(huf),
 
-        muf = rhuf / 60000,
-        rmuf = rhuf % 60000,
+        muf = rhuf / (60 * 1000),
+        rmuf = rhuf % (60 * 1000),
         minutes = Math.floor(muf),
 
         suf = rmuf / 1000,
         rsuf = rmuf % 1000,
         seconds = Math.floor(suf);
 
-    return "There are " + days + " days, " + hours + " hours, " + minutes + " minutes, and " + seconds + " seconds until the end date.";
+    return "There are " + days + " days, " + hours + " hours, " + minutes + " minutes, and " + seconds + " seconds until my next birthday";
 
 }
