03c2b5871c6ac5bd6f60ee4b663f1438aa6bb769
Matthew J Gao <mg00805@srcs.loc>
2017-11-27 11:36:12 -0800

    not working, have no idea why

--- functions.js
+++ functions.js
@@ -21,15 +21,15 @@ function dateMessage() {
     var year = today.getFullYear();
     document.write(" Today date is ", listday[day], " ", listmonth[month], " ", ddate + ", ", year);
 
-    //return "Today is " + day + ", " + month + " " + ddate;
+    return "Today is " + day + ", " + month + " " + ddate;
 
-        var day = today.getDay();
-        var ddate = today.getDate();
-        var listday = ["Sunday", "Monday", "Tuesday", "Wedenesday", "Thursday", "Friday", "Saturday"]
-        var month = today.getMonth() + 1;
-        var listmonth = ["December", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November"];
-        var year = today.getFullYear();
-        return "Today is " + listday[day] + ", " + listmonth[month] + " " + ddate + ", " + year
+    var day = today.getDay();
+    var ddate = today.getDate();
+    var listday = ["Sunday", "Monday", "Tuesday", "Wedenesday", "Thursday", "Friday", "Saturday"]
+    var month = today.getMonth() + 1;
+    var listmonth = ["December", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November"];
+    var year = today.getFullYear();
+    return "Today is " + listday[day] + ", " + listmonth[month] + " " + ddate + ", " + year
 
 }
 
@@ -38,22 +38,19 @@ function dateMessage() {
 
 function birthdaycountDown(birthdate) {
 
-
     var today = new Date(),
         bd = new Date(birthdate),
 
-        // age = today.getYear() - bd.getYear(),
+        year = today.getYear();
+        bd.setFullYear(year);
+    var timecheck = (today.getTime() - bd.getTime());
 
-        bd = bd.setFullYear(today.getYear() + 1, 0, 21),
+    if (timecheck > 0) {
+        bd.setFullYear(year + 1);
+    }
 
-        end = new Date(birthdate),
-        date = today.getDate(),
-        datee = end.getDate(),
-
-        end = new Date(bd),
-
-        duf = (end.getTime() - today.getTime()) / (24 * 60 * 60 * 1000),
-        rduf = (end.getTime() - today.getTime()) % (24 * 60 * 60 * 1000),
+    var duf = (bd.getTime() - today.getTime()) / (24 * 60 * 60 * 1000),
+        rduf = (bd.getTime() - today.getTime()) % (24 * 60 * 60 * 1000),
         days = Math.floor(duf),
 
         huf = rduf / (60 * 60 * 1000),
@@ -62,13 +59,8 @@ function birthdaycountDown(birthdate) {
 
         muf = rhuf / (60 * 1000),
         rmuf = rhuf % (60 * 1000),
-        minutes = Math.floor(muf),
-
-        suf = rmuf / 1000,
-        rsuf = rmuf % 1000,
-        seconds = Math.floor(suf);
+        minutes = Math.floor(muf);
 
     return "There are " + days + " day(s), " + hours + " hour(s), and " + minutes + " minute(s) until my next birthday";
-
 
 }
